9bab229441d689f487eea7344b0966462bc96095
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-22 19:17:59 +0800

    CHARACTER

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,6 @@ script="$script --name $INPUT_CONTAINER_NAME"
 
 script="$script -p $INPUT_PORT_MAPPING"
 
+script="$script --character-set-server=$INPUT_CHARACTER_SET --collation-server=$INPUT_COLLATION"
+
 echo "$script"
