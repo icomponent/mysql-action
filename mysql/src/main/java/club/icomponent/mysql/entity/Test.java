8e69c71cba2a8792539df35916def5f1f2964877
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-23 09:18:33 +0800

    init-mysql-project

---
+++ mysql/src/main/java/club/icomponent/mysql/entity/Test.java
@@ -0,0 +1,25 @@
+package club.icomponent.mysql.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Test() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
